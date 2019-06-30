1a05a8b2bbeaa2155eb49afe89448ec61939f684
babygame0ver <akshatkathpal29@gmail.com>
2019-06-30 17:18:13 +0530

    written system tests

---
+++ tests/test_hardware.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from hardware.calc_hardware import add
+
+print(add(2,3))
+
+
+def main():
+    import config
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_software.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# import config
+
+from software.calc_software import add
+
+print(add(2,3))
