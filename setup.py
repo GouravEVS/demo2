516476ba9e824f41647147cc8241422148fa6f46
babygame0ver <akshatkathpal29@gmail.com>
2019-08-06 00:01:43 +0530

    completed setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+from setuptools import find_packages
+from distutils.core import setup
+
+setup(
+        name="Calculator",
+        version="0.1",
+        packages=find_packages(),
+        license="MIT",
+        long_description=open("README.md").read()
+)
