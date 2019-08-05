76a85106985472c0241dcbf9caceac7022f0c6a5
babygame0ver <akshatkathpal29@gmail.com>
2019-08-05 21:56:27 +0530

    updated repo

--- hardware/tests/test_calc_hardware.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-# import config
-# import calc # no need in the discover command
-from hardware.calc_hardware import addition , div,multiply,subtract
-
-print(addition(2,3))
--- software/tests/test_calc_software.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-from software.calc_software import addition , div,multiply,subtract
-
-print(addition(2,3))
