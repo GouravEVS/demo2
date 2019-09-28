5f613d53b07fe567ef2cb6de427a368622ac8f23
babygame0ver <Akshatkathpal29@gmail.com>
2019-09-28 15:40:23 +0000

    made some changes to test

--- tests/test_software.py
+++ tests/test_software.py
@@ -29,7 +29,7 @@ class TestCalcSoftware(unittest.TestCase):
         self.assertEqual(software_subtract(5,3),2)
 
     def test_software_multiply(self):
-        self.assertEqual(software_multiply(2,3),6)
+        self.assertEqual(software_multiply(2,3),7)
 
     def test_software_division(self):
         self.assertEqual(software_division(10,5),2)
