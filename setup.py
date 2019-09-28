baba92a8798bde0216a9a24f3d597d1adf5d9e92
babygame0ver <akshatkathpal29@gmail.com>
2019-09-28 22:39:25 +0530

    changed travis file config

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from distutils.core import setup
 
 setup(
-        name="Calculator",
+        name="calculator",
         version="0.1",
         packages=find_packages(),
         license="MIT",
