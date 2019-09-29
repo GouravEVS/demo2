c5d1cefb33a79a5e0a17588d86b43144df9fba84
babygame0ver <akshatkathpal29@gmail.com>
2019-09-29 10:51:29 +0530

    updated travis

--- tests/calculator.py
+++ tests/calculator.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
 
-from calculator.software.calc_software import software_addition , software_subtract , software_multiply , software_division
-from calculator.hardware.calc_hardware import hardware_addition , hardware_subtract , hardware_multiply , hardware_division
-
 
 def main():
+    from calculator.software.calc_software import software_addition , software_subtract , software_multiply , software_division
+    from calculator.hardware.calc_hardware import hardware_addition , hardware_subtract , hardware_multiply , hardware_division
     print(software_addition(10,5))
     print(software_subtract(10,5))
     print(software_multiply(10,5))
